1cbbd40c07b7ddabb3a7f2a60e60bfbc2d9bac4a
postelu <romain.postel@etu.univ-lorraine.fr>
2016-11-15 14:51:39 +0100

    CreateEvent done

--- app/dependency.php
+++ app/dependency.php
@@ -91,6 +91,13 @@ $container['App\Controllers\UserController'] = function ($c) {
 		$c->get('App\Repositories\UserRepository')
     );
 };
+
+$container['App\Controllers\EventController'] = function ($c) {
+    return new App\Controllers\EventController(
+        $c
+    );
+};
+
 # -----------------------------------------------------------------------------
 # Factories Models
 # -----------------------------------------------------------------------------
--- app/route.php
+++ app/route.php
@@ -7,3 +7,6 @@
 $app->get('/', 'App\Controllers\HomeController:dispatch')->setName('homepage');
 
 $app->get('/users', 'App\Controllers\UserController:dispatch')->setName('userpage');
+
+$app->get('/createevent','App\Controllers\EventController:formEvent')->setName('formulaire');
+$app->post('/createevent','App\Controllers\EventController:saveEvent');
