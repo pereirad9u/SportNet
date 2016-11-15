d786114c172e229f8ff51dc61990474af9e4d1cf
Guillaume Launay <g.launay96@gmail.com>
2016-11-15 15:15:24 +0100

    Home page non co

--- app/src/Controllers/HomeController.php
+++ app/src/Controllers/HomeController.php
@@ -18,5 +18,19 @@ final class HomeController
         $this->logger = $logger;
         $this->model = $user;
     }
-    
+    public function dispatch(Request $request,Response $response, $args){
+        if(isset($_SESSION['uniqid']) && isset($_SESSION['type'])){
+            if ($_SESSION['type'] == 'org'){
+                $this->view->render($response,'hello_org.twig');
+
+            }else{
+                $this->view->render($response,'hello_user.twig');
+
+            }
+
+        }else{
+            $this->view->render($response,'hello.twig');
+
+        }
+    }
 }
