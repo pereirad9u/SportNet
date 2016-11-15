d8ac1ea80872441c078894ff5ff6bf7853c014d3
pereirad9u <alexandre.pereira.da@gmail.com>
2016-11-15 10:25:50 +0100

    oublie de 's'

--- app/src/Models/User.php
+++ app/src/Models/User.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class Users extends Model
 {
     protected $table = "users";
     protected $primaryKey = "id";
