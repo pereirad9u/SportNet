d039439f47d4f0739d024191b5673bae5ab31e91
Guillaume Launay <g.launay96@gmail.com>
2016-11-16 14:57:07 +0100

    encore un s en plus

--- app/src/Models/Users.php
+++ app/src/Models/Users.php
@@ -13,7 +13,7 @@ class User extends Model
     public $incrementing = false;
 
     function epreuves(){
-        $this->belongsToMany('App\Models\Epreuves');
+       return $this->belongsToMany('App\Models\Epreuves');
     }
 
 }
